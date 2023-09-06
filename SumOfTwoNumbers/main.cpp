a4d5d4b6259854dae287ec82232c0043ca982569
kevinthomasoc <50811018+kevinthomasoc@users.noreply.github.com>
2023-09-05 18:24:06 -0700

    Add files via upload

---
+++ SumOfTwoNumbers/main.cpp
@@ -0,0 +1,25 @@
+//Kevin Thomas CS1A Chapter 2, P. 109, #1
+
+/**************************************************************
+* SUM OF TWO NUMBERS
+* This program calculates the sum of two numbers, 62 and 99, and stores it in a third variable.
+* Input:
+* integer variable initialized to 62.
+* Integer variable initialized to 99.
+* Output:
+* None, just the sum stored inside of a third variable.
+*
+**************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Initialize first variable and set it equal to 62
+	int firstNum = 62;
+	//Initialize second variable and set it equal to 99
+	int secondNum = 99;
+	//Store the sum of those two numbers inside of the sum variable.
+	int sum = firstNum + secondNum;
+	return 0;
+}
